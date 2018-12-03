afb75a8f7417afb9825ba7d9c88bf4595f45b58d
obinnaeye <obinnaeye@yahoo.com>
2018-12-03 22:36:40 +0100

    Connect jQuery ajax to flask

--- resources/FeatureRequest.py
+++ resources/FeatureRequest.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from flask import Flask, render_template, make_response, jsonify, request
-import simplejson as json
+import json
 from sqlalchemy import or_, and_
 from Model import db, Client, ClientSchema, FeatureRequest, FeatureRequestSchema
 
@@ -11,15 +11,18 @@ feture_request_schema = FeatureRequestSchema()
 
 class FeatureRequestResource(Resource):
   def get(self):
+    print('We are here .....')
     headers = {'Content-Type': 'text/html'}
     return make_response(render_template("index.html"))
   
   def post(self):
     json_data = json.loads(request.data)
-    if not json_data:
-      return {'message': 'No input data provided'}, 400
-    # Validate and deserialize input
-    data, errors = comment_schema.load(json_data)
-    if errors:
-      return {"status": "error", "data": errors}, 422
-    return {"message": "New features"}
+    print(json_data)
+    return json_data
+    # if not json_data:
+    #   return {'message': 'No input data provided'}, 400
+    # # Validate and deserialize input
+    # data, errors = comment_schema.load(json_data)
+    # if errors:
+    #   return {"status": "error", "data": errors}, 422
+    # return {"message": "New features"}
