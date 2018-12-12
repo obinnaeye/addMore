0bdff8ad0f00930826fc4ed18d3fc340d125bb5e
obinnaeye <obinnaeye@yahoo.com>
2018-12-12 14:56:14 +0100

    Update migrate to use correct app file

--- migrate.py
+++ migrate.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from Model import db
-from run import create_app
+from app import create_app
 
 app = create_app('config')
 
